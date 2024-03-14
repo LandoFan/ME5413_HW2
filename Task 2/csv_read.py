6fcae7794b018e92b69290aff52c9180c2cd01c0
nusstu0019 <146154242+nusstu0019@users.noreply.github.com>
2024-03-14 16:48:42 +0800

    Update csv_read.py

--- Task 2/csv_read.py
+++ Task 2/csv_read.py
@@ -8,7 +8,6 @@ f = r"my_odometry_bag/laser_odom_to_init.csv"
 # 读取 CSV 文件
 df = pd.read_csv(f)
 
-# 显示 DataFrame 的前几行
 print(df.head())
 time_data = df['Time']
 positionX = df['pose.pose.position.x']
@@ -22,13 +21,9 @@ orientationW = df['pose.pose.orientation.w']
 # print(time_data)
 # print(positionX)
 # print(orientationX)
-# T = np.array([[0, -1, 0, 0],   #lidar 
+# Trans = np.array([[0, -1, 0, 0],   
 #               [0, 0, -1, -0.08],
-#               [1, 0, 0, 0.27],
-#               [0, 0, 0, 1]]) 
-# Trans = np.array([[0, -1, 0, 0],   #lidar 
-#               [0, 0, -1, -0.08],
-#               [1, 0, 0, 0.27],
+#               [1, 0, 0, -0.27],
 #               [0, 0, 0, 1]]) 
 Trans = np.array([[ 7.533745e-03, -9.999714e-01, -6.166020e-04, -4.069766e-03],
                     [ 1.480249e-02,  7.280733e-04, -9.998902e-01, -7.631618e-02],
